b3df535ae7b47201aee02a7653dad459681cbc56
AbdullahNizami <45478265+AbdullahNizami@users.noreply.github.com>
2019-02-12 15:12:12 +0500

    sidebar and other things are adjusted

--- AdminLTE/src/app/ui/ui.module.ts
+++ AdminLTE/src/app/ui/ui.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, SidebarComponent],
   imports: [
     CommonModule,
     RouterModule
   ],
-  exports: [HeaderComponent, FooterComponent],
+  exports: [HeaderComponent, FooterComponent, SidebarComponent],
 })
 export class UiModule { }
